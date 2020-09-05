29903622d3d456555432c5ebf78b498edef6eeb6
William Terry <Tezza48is@gmail.com>
2020-09-05 19:55:41 +0100

    Can change selected machine

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ const NUM_SCATTERED_QUADS: u32 = 256;
 struct Sprites {
     cursor: Option<Handle<ColorMaterial>>,
     grid_object: HashMap<GridObjectType, Handle<ColorMaterial>>,
+    machine: HashMap<MachineType, Handle<ColorMaterial>>,
 }
 
 struct GridWorld {
@@ -38,39 +39,10 @@ impl Default for GridWorld {
 fn tick_world(
     time: Res<Time>,
     mut world: ResMut<GridWorld>,
-    query: Query<&mut Translation>,
 ) {
     world.tick_timer.tick(time.delta_seconds);
     if world.tick_timer.finished {
-        // Tick the world.
 
-        let mut to_move = Vec::new();
-
-        // Check which can be moved
-        for i in 0..world.objects.len() {
-            let object = &world.objects[i];
-            let find = world.get_object_at(object.pos - Vec2::new(0., 1.));
-            match find {
-                Some(obj) => {
-                    if *obj != *object {
-                        continue;
-                    }
-                }
-            }
-        }
-
-        // Move the ones which can be moved.
-        for i in 0..to_move.len() {
-            let mut object = &mut world.objects[i];
-
-            object.pos -= Vec2::new(0., 1.);
-            object.pos = object.pos.max(Vec2::zero()).min(Vec2::new(MAX_WORLD_COORD as f32, MAX_WORLD_COORD as f32));
-
-            if let Ok(mut translation) = query.get_mut::<Translation>(object.entity) {
-                translation.set_x(object.pos.x() * CELL_SIZE as f32);
-                translation.set_y(object.pos.y() * CELL_SIZE as f32);
-            }
-        }
 
         world.tick_timer.reset();
     }
@@ -78,7 +50,7 @@ fn tick_world(
 
 impl GridWorld {
     fn place_object(&mut self, object: GridObject) -> bool {
-        if !self.is_location_empty(object.pos) {
+        if let Some(_) = self.get_object_at(object.pos) {
             return false;
         }
         
@@ -96,33 +68,16 @@ impl GridWorld {
 
         return None
     }
-
-    fn is_location_empty(&self, location: Vec2) -> bool {
-        if  location.x() < 0. ||
-            location.x() > MAX_WORLD_COORD as f32 ||
-            location.y() < 0. ||
-            location.y() > MAX_WORLD_COORD as f32
-        {
-            return false;
-        }
-
-        for obj in self.objects.iter() {
-            if obj.pos.x() == location.x() && obj.pos.y() == location.y() {
-                return false;
-            }
-        }
-
-        true
-    }
 }
 
 // impl GridWorld {
 //     fn
 // }
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum MachineType {
     ConveyerBelt,
-    Target(u8)
+    Target(u8),
 }
 
 struct Machine {
@@ -132,13 +87,19 @@ struct Machine {
 }
 
 
+struct MachinePlacementWidget {
+    dir: u8,
+    entity: Entity,
+    selected_machine: MachineType,
+}
+
 struct GridObject {
     kind: GridObjectType,
     entity: Entity,
     pos: Vec2,
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 enum GridObjectType {
     Cheese = 0x1,
 }
@@ -149,7 +110,7 @@ fn main() {
         .add_resource(GridWorld::default())
         .add_resource(Sprites::default())
         .add_startup_system(init_scene.system())
-        // .add_startup_system(place_random_cubes.system())
+        .add_startup_system(place_random_cubes.system())
         .add_system(update_cursor.system())
         .add_system(debug_place_item.system())
         .add_system(tick_world.system())
@@ -164,14 +125,32 @@ fn init_scene(
     mut color_materials: ResMut<Assets<ColorMaterial>>,
     mut standard_materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    asset_server.load_asset_folder("assets");
+    asset_server.load_asset_folder("assets").unwrap();
 
     sprites.cursor = Some(color_materials.add(asset_server.get_handle("assets/sprites/grid_cell.png").unwrap().into()));
     sprites.grid_object.insert(GridObjectType::Cheese, color_materials.add(asset_server.get_handle("assets/sprites/cheese.png").unwrap().into()));
+    
+    sprites.machine.insert(MachineType::ConveyerBelt, color_materials.add(asset_server.get_handle("assets/sprites/conveyor.png").unwrap().into()));
+    sprites.machine.insert(MachineType::Target(0), color_materials.add(asset_server.get_handle("assets/sprites/target.png").unwrap().into()));
+
+    let widget = commands
+        .spawn(SpriteComponents {
+            material: sprites.machine[&MachineType::ConveyerBelt].clone(),
+            translation: (Vec3::new(-8., 4., 0.) * CELL_SIZE as f32).into(),
+            ..Default::default()
+        }).current_entity().unwrap();
+    
+    commands.insert_resource(MachinePlacementWidget {
+        dir: 0x0,
+        entity: widget,
+        selected_machine: MachineType::ConveyerBelt,
+    });
 
     let camera_entity = commands
         .spawn(Camera2dComponents::default())
         .current_entity().unwrap();
+
+    commands.push_children(camera_entity, &[widget]);
     
     let cursor_entity = commands
         .spawn(SpriteComponents {
@@ -188,43 +167,45 @@ fn init_scene(
             cursor: cursor_entity,
         });
 
-    // let bounds_mesh = meshes.add(Mesh::from(shape::Quad { size: Vec2::new(CELL_SIZE / 2., (MAX_WORLD_COORD + 1.) * CELL_SIZE), flip: false }));
-    // let bounds_mat = standard_materials.add(Color::BLUE.into());
-    // standard_materials.get_mut(&bounds_mat).unwrap().shaded = false;
+    let bounds_mesh = meshes.add(Mesh::from(shape::Quad { size: Vec2::new(CELL_SIZE as f32 / 2., (MAX_WORLD_COORD as f32 + 1.) * CELL_SIZE as f32), flip: false }));
+    let bounds_mat = standard_materials.add(Color::BLUE.into());
+    standard_materials.get_mut(&bounds_mat).unwrap().shaded = false;
         
-    // commands
-    //     .spawn(PbrComponents {
-    //         mesh: bounds_mesh.clone(),
-    //         material: bounds_mat.clone(),
-    //         ..Default::default()
-    //     })
-    //     .with(Translation(Vec3::new(- 24., MAX_WORLD_COORD * CELL_SIZE / 2., 0.)));
+    let max_pos = (MAX_WORLD_COORD * CELL_SIZE) as f32;
 
-    // commands
-    //     .spawn(PbrComponents {
-    //         mesh: bounds_mesh.clone(),
-    //         material: bounds_mat.clone(),
-    //         ..Default::default()
-    //     })
-    //     .with(Translation(Vec3::new(MAX_WORLD_COORD * CELL_SIZE + 24., MAX_WORLD_COORD * CELL_SIZE / 2., 0.)));
+    commands
+        .spawn(PbrComponents {
+            mesh: bounds_mesh.clone(),
+            material: bounds_mat.clone(),
+            ..Default::default()
+        })
+        .with(Translation(Vec3::new(- 24., max_pos / 2., 0.)));
 
-    // commands
-    //     .spawn(PbrComponents {
-    //         mesh: bounds_mesh.clone(),
-    //         material: bounds_mat.clone(),
-    //         ..Default::default()
-    //     })
-    //     .with(Translation(Vec3::new(MAX_WORLD_COORD * CELL_SIZE / 2., - 24., 0.)))
-    //     .with(Rotation(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)));
+    commands
+        .spawn(PbrComponents {
+            mesh: bounds_mesh.clone(),
+            material: bounds_mat.clone(),
+            ..Default::default()
+        })
+        .with(Translation(Vec3::new(max_pos + 24., max_pos / 2., 0.)));
 
-    // commands
-    //     .spawn(PbrComponents {
-    //         mesh: bounds_mesh.clone(),
-    //         material: bounds_mat.clone(),
-    //         ..Default::default()
-    //     })
-    //     .with(Translation(Vec3::new(MAX_WORLD_COORD * CELL_SIZE / 2., MAX_WORLD_COORD * CELL_SIZE + 24., 0.)))
-    //     .with(Rotation(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)));
+    commands
+        .spawn(PbrComponents {
+            mesh: bounds_mesh.clone(),
+            material: bounds_mat.clone(),
+            ..Default::default()
+        })
+        .with(Translation(Vec3::new(max_pos / 2., - 24., 0.)))
+        .with(Rotation(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)));
+
+    commands
+        .spawn(PbrComponents {
+            mesh: bounds_mesh.clone(),
+            material: bounds_mat.clone(),
+            ..Default::default()
+        })
+        .with(Translation(Vec3::new(max_pos / 2., max_pos + 24., 0.)))
+        .with(Rotation(Quat::from_rotation_z(std::f32::consts::FRAC_PI_2)));
 
         
     // Grid background
@@ -265,29 +246,50 @@ fn place_random_cubes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, 
 
 // TODO WT: Make these event triggered, probably a better idea
 fn debug_place_item(
-    mut commands: Commands,
+    // mut commands: Commands,
+    mut machine_widget: ResMut<MachinePlacementWidget>,
     sprites: Res<Sprites>,
     kb: Res<Input<KeyCode>>, 
-    cursor: Res<Cursor>, 
-    mut world: ResMut<GridWorld>
+    // cursor: Res<Cursor>, 
+    // mut world: ResMut<GridWorld>,
+    query: Query<&mut Handle<ColorMaterial>>,
 ) {
-    if kb.just_pressed(KeyCode::Key1) {
-        // TODO WT: Check that object can be placed here.
-        let entity = commands.spawn(SpriteComponents {
-            material: sprites.grid_object.get(&GridObjectType::Cheese).unwrap().clone(),
-            translation: Translation::new(cursor.pos.x() * CELL_SIZE as f32, cursor.pos.y() * CELL_SIZE as f32, 1.),
-            rotation: Quat::from_rotation_z(random::<f32>()).into(),
-            ..Default::default()
-        }).current_entity().unwrap();
+    let selected = 
+             if kb.just_pressed(KeyCode::Key1) { Some(MachineType::ConveyerBelt) }
+        else if kb.just_pressed(KeyCode::Key2) { Some(MachineType::Target(0))}
+        else { None };
 
-        // TODO WT: Make this so the entity can be spawned after the check for whether there's space.
+    if let Some(selected) = selected {
+        if selected != machine_widget.selected_machine {
+            machine_widget.selected_machine = selected;
 
-        if !world.place_object(GridObject {
-            kind: GridObjectType::Cheese,
-            entity,
-            pos: cursor.pos,
-        }) {
-            commands.despawn(entity);
+            println!("Selected {:?}", &selected);
+            
+            if let Ok(mut sprite) = query.get_mut::<Handle<ColorMaterial>>(machine_widget.entity) {
+                *sprite = sprites.machine[&selected];
+                println!("Changing selection");
+            }
         }
     }
+
+
+    // if kb.just_pressed(KeyCode::Key1) {
+    //     // TODO WT: Check that object can be placed here.
+    //     let entity = commands.spawn(SpriteComponents {
+    //         material: sprites.grid_object.get(&GridObjectType::Cheese).unwrap().clone(),
+    //         translation: Translation::new(cursor.pos.x() * CELL_SIZE as f32, cursor.pos.y() * CELL_SIZE as f32, 1.),
+    //         rotation: Quat::from_rotation_z(random::<f32>()).into(),
+    //         ..Default::default()
+    //     }).current_entity().unwrap();
+
+    //     // TODO WT: Make this so the entity can be spawned after the check for whether there's space.
+
+    //     if !world.place_object(GridObject {
+    //         kind: GridObjectType::Cheese,
+    //         entity,
+    //         pos: cursor.pos,
+    //     }) {
+    //         commands.despawn(entity);
+    //     }
+    // }
 }
